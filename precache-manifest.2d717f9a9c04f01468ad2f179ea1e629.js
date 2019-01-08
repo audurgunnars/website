6d7e0f8ceb2f9209384df68979edaf59c9adf1c3
Auður Gunnarsdóttir <audurgunnarsdottir@Auurs-MacBook-Pro.local>
2019-01-08 11:54:44 +0100

    Updates

---
+++ precache-manifest.2d717f9a9c04f01468ad2f179ea1e629.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df8eefd1f2dbc5f3d248",
+    "url": "/website/static/js/runtime~main.df8eefd1.js"
+  },
+  {
+    "revision": "fa5a02284f162f863587",
+    "url": "/website/static/js/main.fa5a0228.chunk.js"
+  },
+  {
+    "revision": "8d15cb4d6767397d8890",
+    "url": "/website/static/js/1.8d15cb4d.chunk.js"
+  },
+  {
+    "revision": "fa5a02284f162f863587",
+    "url": "/website/static/css/main.656274e8.chunk.css"
+  },
+  {
+    "revision": "4f1bb252438aa038a4b1909a6c496d00",
+    "url": "/website/index.html"
+  }
+];
